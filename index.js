14611bd8153dce899763f0c99af581634c54c10e
Ally Netzel <netzelally@gmail.com>
2018-11-30 18:51:00 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-
+class Driver {
+  constructor(name, startDate){
+    this.name = name;
+    this.startDate = new Date(startDate);
+  }
+  
+ yearsExperienceFromBeginningOf(endDate) {
+    return endDate - this.startDate.getFullYear();
+  }
+}
+class Route {
+  constructor(beginningLocation, endingLocation){
+    this.beginningLocation = beginningLocation;
+    this.endingLocation = endingLocation;
+  }
+  blocksTravelled(){
+    
+  }
+}
